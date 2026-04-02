faf83991590f94a457849426394cf5da5bdf900a
ecvivar <ecvivar@gmail.com>
2026-04-02 16:40:11 -0300

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ from flask.helpers import send_from_directory
 import re,os
 from datetime import datetime
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-
 app = Flask(__name__)
 
 mysql = MySQL()
@@ -456,5 +452,5 @@ def process_payment():
    
     return redirect(payment)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
